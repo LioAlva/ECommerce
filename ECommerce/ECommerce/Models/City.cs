87bd0b273889ab33b5beeff5bbc19312fd742af5
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-07-25 20:11:22 -0500

    Departments and it's basic CRUD

---
+++ ECommerce/ECommerce/Models/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "the field {0} is requiered")]
+        [MaxLength(50, ErrorMessage = "the filed {0} must be maximo {1} characters lenght")]
+        [Display(Name = "City")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "the field {0} is requiered")]
+        public int DepartmentId { get; set; }
+
+        public virtual  Department Department { get; set; }
+    }
+}
---
+++ ECommerce/ECommerce/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage ="the field {0} is requiered")]
+        [MaxLength(50,ErrorMessage ="the filed {0} must be maximo {1} characters lenght")]
+        [Display(Name="Department")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
